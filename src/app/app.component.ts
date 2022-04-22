1249b322b5c74bb5b5e45c94a9b82665a3c51bd3
Nithiya03 <nithiyasrirud03@gmail.com>
2022-04-22 17:58:46 +0530

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
 
+  
+import { Component } from '@angular/core';
+import { User} from './user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'form';
+  title = 'tdfForm';
+  topics =['angular','react','php'];
+  userModel=new User('','18euee065@skcet.ac.in',635241789,'');
+  onSubmit()
+  {
+    console.log(this.userModel);
+  }
 }
