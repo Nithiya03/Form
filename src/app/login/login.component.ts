cc64ddb925bfd6c23b238a8201e43218e6ecb397
Nithiya03 <nithiyasrirud03@gmail.com>
2022-04-26 17:48:49 +0530

    angular

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LoginService } from '../login/login.service';
 import { User } from './user1';
@@ -14,19 +14,26 @@ export class LoginComponent {
 
   title = 'tdfForm';
   topics =['angular','react','php'];
-  constructor(private router:Router,private login:LoginService,private fb: FormBuilder,private route: ActivatedRoute){}
-  userModel = this.fb.group({
-    name: [''],
+  userModel: any;
+  // userModel = new User('','',0,'','');
+  constructor(private router:Router,private login:LoginService,private fb: FormBuilder,private route: ActivatedRoute){
+  this.userModel = this.fb.group({
+    name: ['', [Validators.required]],
     mobile: [''],
-    email: [''],
-    gender: [''],
-    topic:['']
-
+    email: ['', [Validators.required, Validators.email]],
+    gender: ['', [Validators.required]],
+    // id: [0, [Validators.required]],
+    topic: ['', [Validators.required]],
   });
-  setMessage(userModel){
-    // this.loginService.adduser(this.userForm.value);
-    this.LoginService.addUser(this.userModel.value);
-    this.router.navigate(['table']);
-  }
+}
+
+  // setMessage(userModel:User){
+  //   // this.loginService.adduser(this.userForm.value);
+  //   this.login.setMessage(this.userModel.value);
+  // }
+  userdata(){  
+    this.login.setMessage(this.userModel.value);
+  
+    this.router.navigate(['/table']);}
 
 }
